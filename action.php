4236ec539800ed48e620ab2c29530853e9287e8d
GanatraJay2000 <jbg@somaiya.edu>
2019-12-09 12:40:29 +0530

    connecting database

---
+++ action.php
@@ -0,0 +1,19 @@
+<?php 
+	echo "Hello World";
+	$name=$_POST['name'];//takes value from form
+	$email=$_POST['email'];
+	$feedback=$_POST['feedback'];
+
+	echo $name."<br>".$email."<br>".$feedback."<br>";//prints the details again
+
+	$con=mysqli_connect("localhost","root","root","sample");
+	if($con){
+		echo "Connected Successfully.<br>";
+		
+		$sql="INSERT INTO user_details VALUES('$name','$email','$feedback')";
+
+		$data=mysqli_query($con,$sql);
+		echo "<br>".$data;
+	}
+		
+?>
